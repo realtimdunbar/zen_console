4a2eeb8a40ad6b8df6aa8a5ec60a54a134278435
realtimdunbar <timothy.c.dunbar@outlook.com>
2018-08-06 19:52:38 -0400

    updated main.go with beginning of outline

--- main.go
+++ main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+
+	// return struct for json
+
+	// store struct for yaml
+
 }
+
+// func IndexModels() {
+// 	pulls appropriate repo from github
+//  searches returned file for parent elements with value from query
+//  returns parents and children from above search
+// }
+
+// func ShowModel() {}
+
+// func CreateModel() {}
+
+// func UpdateModel() {}
+
+// func DestroyModel() {}
